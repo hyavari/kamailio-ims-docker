ff9b7c34238578762ac24b576a03c5021a8ae77a
H.Yavari <hyavari@senasoftware.com>
2024-03-26 00:34:39 -0700

    .

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /usr/local/kamailio/
-./sbin/kamailio -f ./etc/kamailio/pcscf/kamailio.cfg -P /kamailio_pcscf.pid -DD -E -e
+./sbin/kamailio -f etc/kamailio/pcscf/kamailio.cfg -P /kamailio_pcscf.pid -DD -E -e
