800f02990b2d412bc6ec1c0876c6a1a4293b2d1f
H.Yavari <hyavari@senasoftware.com>
2024-05-28 13:42:14 -0700

    .

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /usr/local/kamailio/etc/kamailio/pcscf.cfg
-sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g'/usr/local/kamailio/etc/kamailio/pcscf.cfg
-
 cp -r /usr/local/kamailio/etc/kamailio/pcscf/* /usr/local/kamailio/etc/kamailio/ && \
 rm -rf /usr/local/kamailio/etc/kamailio/pcscf && \
 rm -f /kamailio_pcscf.pid && \
+
+sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /usr/local/kamailio/etc/kamailio/pcscf.cfg
+sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g'/usr/local/kamailio/etc/kamailio/pcscf.cfg
+
 /usr/local/kamailio/sbin/kamailio -f /usr/local/kamailio/etc/kamailio/kamailio.cfg -P /kamailio_pcscf.pid -DD -E -e
