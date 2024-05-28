a8c50c480f01822af8676f0a2fdcda94cdcbaa63
H.Yavari <hyavari@senasoftware.com>
2024-05-28 13:36:53 -0700

    change ips to env

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /usr/local/kamailio/etc/kamailio/pcscf.cfg
+sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g'/usr/local/kamailio/etc/kamailio/pcscf.cfg
+
 cp -r /usr/local/kamailio/etc/kamailio/pcscf/* /usr/local/kamailio/etc/kamailio/ && \
 rm -rf /usr/local/kamailio/etc/kamailio/pcscf && \
 rm -f /kamailio_pcscf.pid && \
