ff617abbb142e0f2bbef2018d2466b6ea0a1519f
H.Yavari <hyavari@senasoftware.com>
2024-06-06 14:55:41 -0700

    improve the entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,17 @@ PID_PATH=/var/run
 # Start Kamailio with the appropriate configuration file
 if [[ -z "$COMPONENT_NAME" ]]; then
     echo "Error: COMPONENT_NAME environment variable not set"; exit 1;
-elif [[ "$COMPONENT_NAME" =~ ^i-?cscf-[[:digit:]]*$ ]]; then
+elif [[ "$COMPONENT_NAME" =~ ^i-?cscf-?[[:digit:]]*$ ]]; then
     echo "Deploying component: '$COMPONENT_NAME'"
     mkdir -p $PID_PATH/kamailio_icscf && \
     rm -f $PID_PATH/kamailio_icscf/kamailio_icscf.pid && \
     $RUN_PATH/kamailio -f $CFG_PATH/icscf/kamailio.cfg -P $PID_PATH/kamailio_icscf/kamailio_icscf.pid -DD -E -e
-elif [[ "$COMPONENT_NAME" =~ ^s-?cscf-[[:digit:]]*$ ]]; then
+elif [[ "$COMPONENT_NAME" =~ ^s-?cscf-?[[:digit:]]*$ ]]; then
     echo "Deploying component: '$COMPONENT_NAME'"
     mkdir -p $PID_PATH/kamailio_scscf && \
     rm -f $PID_PATH/kamailio_scscf/kamailio_scscf.pid && \
     $RUN_PATH/kamailio -f $CFG_PATH/scscf/kamailio.cfg -P $PID_PATH/kamailio_scscf/kamailio_scscf.pid -DD -E -e
-elif [[ "$COMPONENT_NAME" =~ ^p-?cscf-[[:digit:]]*$ ]]; then
+elif [[ "$COMPONENT_NAME" =~ ^p-?cscf-?[[:digit:]]*$ ]]; then
     echo "Deploying component: '$COMPONENT_NAME'"
     mkdir -p $PID_PATH/kamailio_pcscf && \
     rm -f $PID_PATH/kamailio_pcscf/kamailio_pcscf.pid && \
